29b45d53f7bedcc3b6a7298bd118e7406486cbcf
erenbg1 <bades97588@gmail.com>
2025-03-30 19:59:58 +0200

    Delete scripts/Mann-Whitney U Test.R

--- scripts/Mann-Whitney U Test.R
+++
@@ -1,6 +0,0 @@
-# Extract UK and Non-UK order values
-uk_orders <- invoice_data %>% filter(Group == "UK") %>% pull(OrderValue)
-non_uk_orders <- invoice_data %>% filter(Group == "NonUK") %>% pull(OrderValue)
-
-# Mann-Whitney U test (Wilcoxon rank sum)
-wilcox.test(uk_orders, non_uk_orders, alternative = "two.sided")
